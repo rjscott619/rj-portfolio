82d78fd50310c155d5a084db0fbfd0698c142009
rudolph.scott <rudolph.scott@servicenow.com>
2025-10-24 17:22:54 -0700

    updated some chages

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/rj-portfolio/',
   server: { proxy: { '/api': 'http://localhost:3001' } }
 })
